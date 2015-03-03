7b4a5c486a3e3a074a2f9b2a6968aed36f20b9b3
nuen <hello@ericdesigns.ca>
2015-03-03 22:35:52 +0000

    namespace working

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ RottenMangoes::Application.routes.draw do
   resources :movies do
     resources :reviews, only: [:new, :create]
   end
-  resources :users, only: [:new, :create]
+  resources :users
+
+  namespace :admin do
+    resources :users
+  end
+
   resources :sessions, only: [:new, :create, :destroy]
   root to: 'movies#index'
 end

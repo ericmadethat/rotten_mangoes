37e858533418252e6dae75fe318fff12be29e1a2
nuen <hello@ericdesigns.ca>
2015-03-06 01:42:30 +0000

    Admins can delete users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -36,6 +36,12 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path, notice: "User has been deleted"
+  end
+
   protected
 
   def check_admin

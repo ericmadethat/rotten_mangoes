0ed8577974117f33f7bdb6eec7ab4845b578efe7
nuen <hello@ericdesigns.ca>
2015-03-06 00:49:05 +0000

    Admins can create and edit users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,40 @@ class Admin::UsersController < ApplicationController
 
   before_action :check_admin
 
+  def index
+    @users = User.all.page(params[:page]).per(3)
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id]) if params[:id]
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: "#{@user.firstname} was created successfully!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to admin_user_path(@user), notice: "User information updated succesfully."
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   protected
 
   def check_admin
@@ -15,7 +49,7 @@ class Admin::UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(
-      :email, :firstname, :lastname, :admin
+      :email, :firstname, :lastname, :admin, :password, :password_confirmation
     )
   end
 

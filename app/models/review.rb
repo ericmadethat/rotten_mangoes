85e24b631f31233ad94ce237d2c0aa106cdae0f5
Eric Nguyen <hello@ericdesigns.ca>
2015-04-12 15:46:46 -0700

    Minor spacing fixes

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :movie
 
-    validates :user,
+  validates :user,
     presence: true
 
   validates :movie,
@@ -13,9 +13,10 @@ class Review < ActiveRecord::Base
 
   validates :rating_out_of_ten,
     numericality: { only_integer: true }
+
   validates :rating_out_of_ten,
     numericality: { greater_than_or_equal_to: 1 }
+    
   validates :rating_out_of_ten,
     numericality: { less_than_or_equal_to: 10 }
-  
 end
